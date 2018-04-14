dd01de8d3626c4fe6c820e02f6483eceee6b1c87
Brandon Gray <grayba@gmail.com>
2018-04-14 17:53:42 -0400

    Resources + Login page update
    -Updated resources page
    -Updated login page with link to ORCC

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import FontIcon from 'material-ui/FontIcon';
 import PropTypes from 'prop-types';
 import { TextField, RaisedButton } from 'material-ui';
 
 const buttonStyle = {
   margin: 12,
 };
+
+const contactStyle = {
+  float:'left',
+  margin:0,
+  marginLeft:10,
+  marginTop:-10
+}
 
 class Login extends Component {
   state = {
@@ -40,13 +48,19 @@ class Login extends Component {
           style={buttonStyle}
           label="Log in/Sign up"
         />
+        <div style={{marginTop:50}}>
+          <p><a href="tel:16135622333"><FontIcon className="material-icons" style={{float:'left'}}>phone</FontIcon></a>
+          <p style={contactStyle}>Need counselling support?
+          <br />Call the Crisis Line.</p>
+          </p>
+        </div>
       </div>
     );
   }
 }
 
 Login.defaultProps = {
-  loginCB: () => {},
+  loginCB: () => { },
 };
 
 Login.propTypes = {
