34d63aa40ee6c0d8a494059214e3512ca1f3117e
Sopheap Hok <sopheap.hok@rebel.com>
2018-04-14 19:55:08 -0400

    more list stuff

--- src/components/IncidentSummary.js
+++ src/components/IncidentSummary.js
@@ -1,4 +1,4 @@
-// import { } from 'material-ui';
+import { Card, CardHeader, CardText } from 'material-ui';
 import React, { Component } from 'react';
 
 // const showOnlyMyIncidentsCheckboxStyle = {
@@ -15,7 +15,20 @@ class IncidentSummary extends Component {
   render() {
     return (
       <div >
-          card detail
+        <Card>
+          <CardHeader
+            title="Without Avatar"
+            subtitle="Subtitle"
+            actAsExpander
+            showExpandableButton
+          />
+          <CardText expandable>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+          </CardText>
+        </Card>
       </div>
     );
   }
